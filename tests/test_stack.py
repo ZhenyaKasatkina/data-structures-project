4b8c6ba279f167b4174402f00ce8a790f670cc44
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-28 23:29:47 +0300

    change src/stack.py, tests/test_stack.py, pyproject.toml, .gitignore

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,30 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class TestNode(unittest.TestCase):
+    def setUp(self):
+        self.A = Node(data=5)
+        self.B = Node(("one", "two"), "three")
+
+    def test_init(self):
+        self.assertEqual(self.A.data, 5)
+        self.assertEqual(self.A.next_node, None)
+        self.assertEqual(self.B.data, ("one", "two"))
+        self.assertEqual(self.B.next_node, "three")
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.A = Stack()
+
+    def test_init(self):
+        self.assertEqual(self.A.top, None)
+
+    def test_push(self):
+        self.A.push([1, 2, 3, 4])
+        self.A.push(2)
+        self.A.push("item")
+        self.assertEqual(self.A.top.data, "item")
+        self.assertEqual(self.A.top.next_node.next_node.data, [1, 2, 3, 4])
