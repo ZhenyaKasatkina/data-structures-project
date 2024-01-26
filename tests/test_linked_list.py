fbcea792609a6d380712abcd56e720c7b17d0fdb
ZhenyaKasatkina <knopisha.zh@gmail.com>
2024-01-26 20:49:41 +0300

    change src/linked_list.py, tests/test_linked_list.py (согласно файла homework-6/README.md)

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -28,8 +28,8 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(str(self.A), "{'num': 'num'} -> None")
         self.A.insert_beginning({"num": ("one", "five")})
         self.assertEqual(str(self.A), "{'num': ('one', 'five')} -> {'num': 'num'} -> None")
-        # with self.assertRaises(AttributeError):
-        #     fault = self.A.tail.next_node.data
+        with self.assertRaises(AttributeError):
+            x = self.A.tail.next_node.data
 
     def test_insert_at_end(self):
         self.A.insert_at_end({'num': 'num'})
@@ -39,8 +39,25 @@ class TestLinkedList(unittest.TestCase):
 
     def test_str(self):
         self.assertEqual(str(self.A), "None")
-
         self.A.insert_beginning({"num": ("one", "five")})
         self.A.insert_beginning({'num': 'num'})
         self.A.insert_at_end({'num': 'num'})
         self.assertEqual(str(self.A), "{'num': 'num'} -> {'num': ('one', 'five')} -> {'num': 'num'} -> None")
+
+    def test_to_list(self):
+        self.assertEqual(self.A.to_list(), [])
+        self.A.insert_beginning({"num": ("one", "five")})
+        self.A.insert_beginning({'num': 'num'})
+        self.A.insert_at_end({'num': 'num'})
+        self.assertEqual(self.A.to_list(), [{'num': 'num'}, {'num': ('one', 'five')}, {'num': 'num'}])
+
+    def test_get_data_by_id(self):
+        self.A.insert_beginning({"id": ("one", "five")})
+        self.A.insert_beginning([2, 'num'])
+        self.A.insert_at_end({'id': 'num', "one": "five"})
+        self.assertEqual(self.A.get_data_by_id("num"), {'id': 'num', "one": "five"})
+        self.assertEqual(self.A.get_data_by_id("5"), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
